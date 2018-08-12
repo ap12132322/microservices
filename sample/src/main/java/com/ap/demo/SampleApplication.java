090674854eaaa09d500e84d6657295df02e4d8a3
ap12132322 <42326354+ap12132322@users.noreply.github.com>
2018-08-13 04:12:18 +0530

    commit 2
    Implimented command line runner

--- sample/src/main/java/com/ap/demo/SampleApplication.java
+++ sample/src/main/java/com/ap/demo/SampleApplication.java
@@ -1,12 +1,19 @@
 package com.ap.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleApplication {
+public class SampleApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Application started scuuessfully...");
+		
 	}
 }
